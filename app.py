efa9d522251c00f75b78afe7f2e9026a5c8d1264
Chuyi Zhang <cz1302@nyu.edu>
2019-11-08 00:13:30 +0800

    feat: configuration

---
+++ app.py
@@ -0,0 +1,27 @@
+#Import Flask Library
+from flask import Flask, render_template, request, session, url_for, redirect
+import pymysql.cursors
+
+#Initialize the app from Flask
+app = Flask(__name__)
+
+#Configure MySQL
+conn = pymysql.connect(host='192.168.64.2',
+                       user='chuyi',
+                       password='password',
+                       db='blog',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
+
+#Define a route to hello function
+@app.route('/')
+def hello():
+	return render_template('index.html')
+
+
+app.secret_key = 'some key that you will never guess'
+#Run the app on localhost port 5000
+#debug = True -> you don't have to restart flask
+#for changes to go through, TURN OFF FOR PRODUCTION
+if __name__ == "__main__":
+	app.run('localhost', 5000, debug = True)
