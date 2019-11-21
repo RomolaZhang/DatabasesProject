d00c6b7639c46d2f90deadfcc7834c614a9628bc
Harry Wang <zixinwang@Zixins-MacBook-Pro.local>
2019-11-21 23:27:10 +0800

    feat: file structures

---
+++ myapp/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request, session, url_for, redirect
+from database import conn
+from public import public
+from register_login import register_login
+from customer import customer
+
+#Initialize the app from Flask
+app = Flask(__name__)
+
+#public info page
+app.register_blueprint(public)
+
+#login & register page
+app.register_blueprint(register_login)
+
+#customer homepage
+app.register_blueprint(customer)
+
+#staff homepage
+
+#booking agent homepage
+
+app.secret_key = 'some key that you will never guess'
+#Run the app on localhost port 5000
+#debug = True -> you don't have to restart flask
+#for changes to go through, TURN OFF FOR PRODUCTION
+if __name__ == "__main__":
+    app.run('localhost', 5000, debug = True)
---
+++ myapp/customer.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from flask import Blueprint, render_template, request, session, url_for, redirect
+from database import conn
+
+customer = Blueprint('customer', __name__)
+
+@customer.route('/customerHome')
+def customerHome():
+    email = session['email']
+    username = session['username']
+    cursor = conn.cursor();
+    query = 'SELECT * FROM ticket WHERE cust_email = %s'
+    cursor.execute(query, (email))
+    data1 = cursor.fetchall() 
+    for each in data1:
+        print(each['airline_name'],each['flight_num'],each['dept_time'])
+    cursor.close()
+    return render_template('customerHome.html', username = username, flights=data1
